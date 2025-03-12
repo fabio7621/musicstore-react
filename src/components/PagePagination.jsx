4b7bb29cf544c958e9d7ae0c0974a6f6cff7bd6f
fabio7621 <d6200121a@gmail.com>
2025-03-12 23:30:09 +0800

    update button

--- src/components/PagePagination.jsx
+++ src/components/PagePagination.jsx
@@ -2,25 +2,40 @@ export default function PagePagination({ pagination, pagesChange }) {
   return (
     <>
       <nav className="w-100 col mx-auto">
-        <ul className="mx-auto pagination mt-5" style={{ width: "fit-content" }}>
-          <li className={`page-item ${!pagination.has_pre && "disabled"}`} onClick={() => pagesChange(pagination.current_page - 1)}>
-            <a className="page-link" href="#">
-              上一頁
-            </a>
+        <ul
+          className="mx-auto pagination mt-5"
+          style={{ width: "fit-content" }}
+        >
+          <li
+            className={`page-item ${!pagination.has_pre && "disabled"}`}
+            onClick={() => pagesChange(pagination.current_page - 1)}
+          >
+            <button className="page-link">上一頁</button>
           </li>
 
           {Array.from({ length: pagination.total_pages }).map((_, index) => (
-            <li key={index} className={`page-item ${pagination.current_page === index + 1 && "active"}`}>
-              <a onClick={() => pagesChange(index + 1)} className="page-link" href="#">
+            <li
+              key={index}
+              className={`page-item ${
+                pagination.current_page === index + 1 && "active"
+              }`}
+            >
+              <button
+                onClick={() => pagesChange(index + 1)}
+                className="page-link"
+              >
                 {index + 1}
-              </a>
+              </button>
             </li>
           ))}
 
           <li className={`page-item ${!pagination.has_next && "disabled"}`}>
-            <a onClick={() => pagesChange(pagination.current_page + 1)} className="page-link" href="#">
+            <button
+              onClick={() => pagesChange(pagination.current_page + 1)}
+              className="page-link"
+            >
               下一頁
-            </a>
+            </button>
           </li>
         </ul>
       </nav>
