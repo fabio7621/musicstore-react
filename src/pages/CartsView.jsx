8dfe520787c5ea56da56bb6cf8ccb977e9fc09e5
fabio7621 <d6200121a@gmail.com>
2025-03-13 23:53:30 +0800

    cart page not finish keep working gogo

--- src/pages/CartsView.jsx
+++ src/pages/CartsView.jsx
@@ -1,4 +1,97 @@
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import ReactLoading from "react-loading";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_BASE_URL;
+const apiPath = import.meta.env.VITE_API_PATH;
+
 export default function CartPage() {
+  const [cart, setCart] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  // 取得購物車
+  const getCart = async () => {
+    try {
+      const res = await axios.get(`${apiUrl}/v2/api/${apiPath}/cart`);
+      setCart(res.data.data);
+    } catch (error) {
+      alert(`取得購物車失敗: ${error.message}`);
+    }
+  };
+
+  // 清空購物車
+  const removeCart = async () => {
+    setLoading(true);
+    try {
+      await axios.delete(`${apiUrl}/v2/api/${apiPath}/carts`);
+      getCart();
+    } catch (error) {
+      alert(`清除購物車失敗: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // 單項刪除
+  const removeCartItem = async (id) => {
+    setLoading(true);
+    try {
+      await axios.delete(`${apiUrl}/v2/api/${apiPath}/cart/${id}`);
+      getCart();
+    } catch (error) {
+      alert(`刪除購物車單項失敗: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // 更新購物車
+  const updateCart = async (cart_Id, product_id, qty) => {
+    setLoading(true);
+    try {
+      await axios.put(`${apiUrl}/v2/api/${apiPath}/cart/${cart_Id}`, {
+        data: { product_id, qty: Number(qty) },
+      });
+      getCart();
+    } catch (error) {
+      alert(`更新購物車失敗: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // 結帳
+  const checkout = async (data) => {
+    setLoading(true);
+    try {
+      await axios.post(`${apiUrl}/v2/api/${apiPath}/order`, data);
+      reset();
+      getCart();
+    } catch (error) {
+      alert(`訂單失敗: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    const { message, ...user } = data;
+    const userData = { data: { user, message } };
+    checkout(userData);
+  });
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
   return (
     <div>
       {/* 麵包屑導航 */}
@@ -34,7 +127,46 @@ export default function CartPage() {
               </tr>
             </thead>
             <tbody>
-              <tr>
+              {cart.carts?.map((cartItem) => (
+                <tr key={cartItem.id}>
+                  <td>
+                    <div className="shopcart-title d-flex align-items-center">
+                      <div className="shopcart-pic">
+                        <img src={cartItem.product.imageUrl} alt="album" />
+                      </div>
+                      <span>{cartItem.product.title}</span>
+                    </div>
+                  </td>
+                  <td>
+                    <input
+                      className=""
+                      type="text"
+                      name="quantity1"
+                      defaultValue={cartItem.qty}
+                      onChange={(e) =>
+                        updateCart(
+                          cartItem.id,
+                          cartItem.product_id,
+                          e.target.value
+                        )
+                      }
+                    />
+                  </td>
+                  <td>{cartItem.product.price}</td>
+                  <td>
+                    <div
+                      onClick={() => removeCartItem(cartItem.id)}
+                      className="shopcart-del-pic"
+                    >
+                      <img
+                        src="@/../../../../public/icon/Close.png"
+                        alt="delete"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+              {/* <tr>
                 <td>
                   <div className="shopcart-title d-flex align-items-center">
                     <div className="shopcart-pic">
@@ -51,39 +183,17 @@ export default function CartPage() {
                 </td>
                 <td>1220</td>
                 <td>
-                  <div className="shopcart-del-pic">
+                  <div
+                    onClick={() => removeCartItem(cartItem.id)}
+                    className="shopcart-del-pic"
+                  >
                     <img
                       src="@/../../../../public/icon/Close.png"
                       alt="delete"
                     />
                   </div>
                 </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="shopcart-title d-flex align-items-center">
-                    <div className="shopcart-pic">
-                      <img
-                        src="@/../../../../public/music/1444.jpg"
-                        alt="album"
-                      />
-                    </div>
-                    <span>Lisa - ASDAFW</span>
-                  </div>
-                </td>
-                <td>
-                  <input className="" type="text" name="quantity2" />
-                </td>
-                <td>1220</td>
-                <td>
-                  <div className="shopcart-del-pic">
-                    <img
-                      src="@/../../../../public/icon/Close.png"
-                      alt="delete"
-                    />
-                  </div>
-                </td>
-              </tr>
+              </tr> */}
             </tbody>
           </table>
 
@@ -118,3 +228,48 @@ export default function CartPage() {
     </div>
   );
 }
+// {
+//   "success": true,
+//   "data": {
+//     "carts": [
+//       {
+//         "coupon": {
+//           "code": "testCode",
+//           "due_date": 6547658,
+//           "id": "-L9uIs5EfPibJpwwTMhN",
+//           "is_enabled": 1,
+//           "percent": 60,
+//           "title": "超級特惠價格"
+//         },
+//         "final_total": 2160,
+//         "id": "-LATwxc_bIJu-AR4AlNj",
+//         "product": {
+//           "category": "衣服3",
+//           "content": "這是內容",
+//           "description": "Sit down please 名設計師設計",
+//           "id": "-L9tH8jxVb2Ka_DYPwng",
+//           "imageUrl": "主圖網址",
+//           "imagesUrl": [
+//             "圖片網址一",
+//             "圖片網址二",
+//             "圖片網址三",
+//             "圖片網址四",
+//             "圖片網址五"
+//           ],
+//           "is_enabled": 1,
+//           "num": 1,
+//           "origin_price": 500,
+//           "price": 600,
+//           "title": "[賣]動物園造型衣服3",
+//           "unit": "個"
+//         },
+//         "product_id": "-L9tH8jxVb2Ka_DYPwng",
+//         "qty": 6,
+//         "total": 3600
+//       }
+//     ],
+//     "total": 3600,
+//     "final_total": 2160
+//   },
+//   "messages": []
+// }
